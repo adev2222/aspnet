fb472d76afccb5e280562e0db6ee869136b837a1
Abdurahmon K <abbmuslim@gmail.com>
2024-02-18 23:37:00 +0100

    Remove unnecessary code in DbInitializer.cs

--- WEB/Data/DbInitializer.cs
+++ WEB/Data/DbInitializer.cs
@@ -4,7 +4,6 @@ public class DbInitializer
 {
    public static void Initialize(SchoolContext context)
         {
-            context.Database.EnsureCreated();
 
             // Look for any students.
             if (context.Students.Any())
