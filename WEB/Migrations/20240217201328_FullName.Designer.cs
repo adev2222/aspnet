0b54dc65628ecff1fe744c57ad535fd2cc3bf430
Abdurahmon K <abbmuslim@gmail.com>
2024-02-18 23:37:14 +0100

    Add migration for FullName

--- WEB/Migrations/20240217201328_FullName.Designer.cs
+++ WEB/Migrations/20240217201328_FullName.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WEB;
 
@@ -10,9 +11,11 @@ using WEB;
 namespace WEB.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20240217201328_FullName")]
+    partial class FullName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
