05a823da84e780d7d6c386ddd625517dcd94bd2d
Abdurahmon K <abbmuslim@gmail.com>
2024-02-18 23:37:44 +0100

    Add validation and display attributes to Student model

--- WEB/Models/Student.cs
+++ WEB/Models/Student.cs
@@ -1,11 +1,31 @@
-﻿namespace WEB;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WEB;
 
 public class Student
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(50, MinimumLength=2)]
+    [Column("FirstName")]
     public string FirstMidName { get; set; }
+    [StringLength(50, MinimumLength=2)]
+    [Display(Name = "Last Name")]
     public string LastName { get; set; }
+    [Display(Name = "Enrollment Date")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     public DateTime EnrollmentDate { get; set; }
+
+    [Display(Name = "Full Name")]
+    public string FullName
+    {
+        get
+        {
+            return LastName + ", " + FirstMidName;
+        }
+    }
 
     public ICollection<Enrollment>? Enrollments { get; set; }
 
