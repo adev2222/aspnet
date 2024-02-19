12fadca058fa44df6b0f7a833425c51478b1fa86
Abdurahmon K <abbmuslim@gmail.com>
2024-02-19 11:02:07 +0100

    Add InstructorIndexData and Instructor views

---
+++ WEB/Models/ViewModels/InstructorIndexData.cs
@@ -0,0 +1,8 @@
+namespace WEB;
+
+public class InstructionIndexData
+{
+    public IEnumerable<Instructor> Instructors { get; set; }
+    public IEnumerable<Course> Courses { get; set; }
+    public IEnumerable<Enrollment> Enrollments { get; set; }
+}
